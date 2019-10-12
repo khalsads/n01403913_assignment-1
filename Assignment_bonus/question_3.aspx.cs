73d7ac9aae750dc4023c9e57ae3e777c445ba0ac
khalsads <khalsads@gmail.com>
2019-10-12 00:53:54 -0400

    Second Attempt

--- Assignment_bonus/question_3.aspx.cs
+++ Assignment_bonus/question_3.aspx.cs
@@ -22,8 +22,9 @@ namespace Assignment_bonus
                 if (Page.IsValid)
                 {
 
-                    //Clearing if there is any value stored in "value_selected_result"
+                    //Clearing if there is any value in variables
                     ticket_ordered_result.InnerHtml = "";
+                    string MsgOut = "";
 
                     //Storing values submitted by form to server side integers.
                     int Ticket_Ordered = Convert.ToInt32(ticket_ordered.Text);
@@ -34,58 +35,77 @@ namespace Assignment_bonus
                     Double Cost=0.25;
                     Double Total_Cost= Ticket_Ordered * Cost;
 
-                    //Clearing previous stored in MsgOut variable.
-                    string MsgOut = "";
 
-                    //Display value as per user selection
+                    //Standard msg value to display
                     MsgOut = "<br>You received a bundle of ";
 
                     //                    ticket_ordered_result.InnerHtml += MsgOut + Ticket_Ordered.ToString()+"<br>";
 
+                    //Case 1 when tickets are less than and equal to 50
                     if (Ticket_Ordered <= 50)
                     {
                         for (var i=1; i<=Ticket_Ordered; i++) {
                             ticket_ordered_result.InnerHtml += MsgOut + "1! That's " +i.ToString()+ " ticket(s)!";
                         }
+
+                        //Print Total cost of Tickets
                         ticket_ordered_result.InnerHtml += "<br><br>Your total ticket(s) is "+Ticket_Ordered.ToString()+" and your cost is $"+Total_Cost.ToString()+"cad.";
                     }
+
+                    //Case 2 when tickets are more than 50 but less than or equal to 150
                     else if ((Ticket_Ordered > 50) && (Ticket_Ordered <= 150))
                     {
                         Reminder = Ticket_Ordered % 2;
                         Loop = (Ticket_Ordered - Reminder) / 2;
-                        for (var i = 1; i <= Loop; i++)
+                        for (var i = 1; i <= Loop; i++) //For Loop to print tickets bundle
                         {
                             ticket_ordered_result.InnerHtml += MsgOut + "2! That's " +(i*2).ToString()+ " ticket(s)!";
                         }
+
+                        //To print last if remaning tickets or loose tickets less than bundle size
                         if (Reminder > 0)
                         {
                             ticket_ordered_result.InnerHtml += "<br>Your leftover is "+Reminder.ToString()+" ticket(s). That's " + Ticket_Ordered.ToString() + " ticket(s)!";
                         }
+
+                        //Print Total cost of Tickets
                         ticket_ordered_result.InnerHtml += "<br><br>Your total ticket(s) is " + Ticket_Ordered.ToString() + " and your cost is $" + Total_Cost.ToString() + "cad!";
                     }
+
+                    //Case 3 when tickets are more that 150 but less than or equal to 300
                     else if ((Ticket_Ordered > 150) && (Ticket_Ordered <= 300))
                     {
                         Reminder = Ticket_Ordered % 3;
                         Loop = (Ticket_Ordered - Reminder) / 3;
-                        for (var i = 1; i <= Loop; i++)
+                        for (var i = 1; i <= Loop; i++)//For Loop to print tickets bundle
                         {
                             ticket_ordered_result.InnerHtml += MsgOut + "3! That's " +(i*3).ToString()+ " ticket(s)!";
                         }
+
+                        //To print last if remaning tickets or loose tickets less than bundle size
                         if (Reminder > 0) {
                             ticket_ordered_result.InnerHtml += "<br>Your leftover is "+Reminder.ToString()+" ticket(s). That's " + Ticket_Ordered.ToString()+ " ticket(s)!";
                         }
+
+                        //Print Total cost of Tickets
                         ticket_ordered_result.InnerHtml += "<br><br>Your total ticket(s) is " + Ticket_Ordered.ToString() + " and your cost is $" + Total_Cost.ToString() + "cad!";
                     }
+
+                    //Case 4 when tickets are more than 300 
                     else if (Ticket_Ordered > 300) {
                         Reminder = Ticket_Ordered % 5;
                         Loop = (Ticket_Ordered - Reminder) / 5;
-                        for (var i = 1; i <= Loop; i++)
+                        for (var i = 1; i <= Loop; i++)//For Loop to print tickets bundle
                         {
                             ticket_ordered_result.InnerHtml += MsgOut + "5! That's " +(i*5).ToString()+ " ticket(s)!";
                         }
+
+                        //To print last if remaning tickets or loose tickets less than bundle size  
                         if (Reminder > 0) {
                             ticket_ordered_result.InnerHtml += "<br>Your leftover is "+Reminder.ToString()+" ticket(s). That's "+Ticket_Ordered.ToString()+" ticket(s)!";
                         }
+
+                        //Print Total cost of Tickets
                         ticket_ordered_result.InnerHtml += "<br><br>Your total ticket(s) is "+Ticket_Ordered.ToString()+" and your cost is $"+Total_Cost.ToString()+"cad!";
                     }
                 }
